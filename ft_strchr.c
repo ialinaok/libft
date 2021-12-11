ce511392d9d8c22d03c2a030c0b7c8c0e6d26572
ialinaok <alinapielaszkiewicz@gmail.com>
2021-12-11 21:29:23 +0100

    adding working ft_strchr (NULL riddle)

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,31 +6,84 @@
 /*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 15:24:47 by ialinaok          #+#    #+#             */
-/*   Updated: 2021/12/10 21:32:24 by ialinaok         ###   ########.fr       */
+/*   Updated: 2021/12/11 21:26:06 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
+// char	*ft_strchr(const char *str, int c)
+// {
+// 	char	*ptr;
+
+// 	ptr = (char *)str;
+// 	while (*ptr)
+// 	{
+// 		if (*ptr == c)
+// 		{
+// 			return (ptr);
+// 		}
+// 		ptr++;
+// 	}
+// 	if ((*ptr == '\0') && (*ptr == c))
+// 	{
+// 		return (ptr);
+// 	}
+// 	return (NULL);
+// }
+
 char	*ft_strchr(const char *str, int c)
 {
 	char	*ptr;
 
 	ptr = (char *)str;
-	while ((*ptr != '\0') && (*ptr != c))
+	while (*ptr != '\0')
 	{
+		if (*ptr == c)
+		{
+			return (ptr);
+		}
 		ptr++;
 	}
 	if (*ptr == c)
 	{
 		return (ptr);
 	}
-	else
-	{
-		return (ptr);
-	}
+	return (NULL);
 }
+
+// char	*ft_strchr(const char *p, int ch)
+// {
+// 	char	c;
+
+// 	c = ch;
+// 	for (;; ++p) {
+// 		if (*p == c)
+// 			return ((char *)p);
+// 		if (*p == '\0')
+// 			return (NULL);
+// 	}
+// }
+// char	*ft_strchr(const char *s, int c)
+// {
+// 	char *pointer;
+
+// 	pointer = (char *)s;
+// 	while (*pointer)
+// 	{
+// 		if (*pointer == c)
+// 		{
+// 			return (pointer);
+// 		}
+// 		pointer++;
+// 	}
+// 	if (*pointer == '\0' && c == '\0')
+// 	{
+// 		return (pointer);
+// 	}
+// 	return (NULL);
+// }
 
 // int main(void)
 // {

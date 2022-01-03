aaa40113c122b47f6e422f3d02b97b4165967f60
ialinaok <alinapielaszkiewicz@gmail.com>
2022-01-03 15:43:00 +0100

    add back lst

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 20:22:33 by ialinaok          #+#    #+#             */
-/*   Updated: 2021/12/30 20:26:17 by ialinaok         ###   ########.fr       */
+/*   Updated: 2022/01/03 15:42:12 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	last = ft_lstlast(&lst);
+	last = ft_lstlast(*lst);
 	last->next = new;
 }

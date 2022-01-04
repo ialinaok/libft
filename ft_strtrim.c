1e0d38987faec210576225c94f1efb9e42309680
ialinaok <alinapielaszkiewicz@gmail.com>
2022-01-04 20:04:15 +0100

    working strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,11 @@
 /*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 22:22:06 by ialinaok          #+#    #+#             */
-/*   Updated: 2021/12/28 14:26:26 by ialinaok         ###   ########.fr       */
+/*   Updated: 2022/01/04 20:02:49 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "libft.h"
 
 static int	find_end(char const *s1, char const *s2)
@@ -20,6 +19,7 @@ static int	find_end(char const *s1, char const *s2)
 	int	j;
 
 	i = ft_strlen(s1) - 1;
+	j = 0;
 	while (i >= 0 && s2[j] != '\0')
 	{
 		j = 0;
@@ -71,25 +71,26 @@ char	*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	start = find_start(s1, set);
 	len = find_end(s1, set) - start + 2;
-	if (s1 && len >= 0)
+	if (!s1)
+		return (NULL);
+	if (len > 0)
+		trim = (char *)malloc(len);
+	if (len <= 0)
+		trim = (char *)malloc(1);
+	if (!trim)
+		return (NULL);
+	while (len > 1)
 	{
-		trim = malloc(len);
-		while (len > 1)
-		{
-			trim[i] = s1[start];
-			i++;
-			start++;
-			len--;
-		}
-		trim [i] = '\0';
-		return (trim);
+		trim[i++] = s1[start++];
+		len--;
 	}
-	return (NULL);
+	trim [i] = '\0';
+	return (trim);
 }
 
 // int	main(void)
 // {
-// 	char	arr1[] = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ";
+// 	char	arr1[] = "   \t \n\n\nHello \t  Please\n Trim me !\n   \n  ";
 // 	char	set[] = " \n\t";
 // 	char	*trim;
 

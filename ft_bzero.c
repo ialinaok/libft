ba9a3e3b97d5b55d2ad1889e434b8774fe7f4de1
ialinaok <alinapielaszkiewicz@gmail.com>
2022-02-22 16:11:12 +0100

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,41 +6,22 @@
 /*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 15:39:55 by ialinaok          #+#    #+#             */
-/*   Updated: 2021/12/10 21:48:53 by ialinaok         ###   ########.fr       */
+/*   Updated: 2022/02/22 16:10:29 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
+/* function erases n bytes of memory starting at location pointed to by s */
+
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*put;
 
-	put = s;
+	put = str;
 	while (n > 0)
 	{
 		*put++ = '\0';
 		n--;
 	}
 }
-
-// int	main(void)
-// {
-// 	char	arr[5] = "Hola";
-// 	int	n;
-
-// 	n = 0;
-// 	while (arr[n] != '\0')
-// 	{
-// 		printf("%c", arr[n]);
-// 		n++;
-// 	}
-// 	ft_bzero(arr, 2);
-// 	n = 2;
-// 	while (arr[n] != '\0')
-// 	{
-// 		printf("%c", arr[n]);
-// 		n++;
-// 	}
-// 	return (0);
-// }

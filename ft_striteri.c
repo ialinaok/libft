7c8e0059f6a0b14f05e8800eff26f0ac559212e8
ialinaok <alinapielaszkiewicz@gmail.com>
2022-02-22 18:08:05 +0100

    ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,11 +6,15 @@
 /*   By: ialinaok <ialinaok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 15:14:46 by ialinaok          #+#    #+#             */
-/*   Updated: 2022/01/04 20:54:17 by ialinaok         ###   ########.fr       */
+/*   Updated: 2022/02/22 18:07:47 by ialinaok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+/* Applies the function f to each character of the string passed as argument
+ * and passing its index as first argument; each character is passed by
+ * address to f to be modified if necessary */ 
+
+#include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
